a378b353b6bda1a5d4a3fa9efdc94776c6e9fb94
mmills2 <30macm@gmail.com>
2022-04-19 22:07:13 -0400

    ShoppingListFragment RecycleView

--- app/src/main/java/edu/quinnipiac/ser210/thirstmate/ShoppingListFragment.java
+++ app/src/main/java/edu/quinnipiac/ser210/thirstmate/ShoppingListFragment.java
@@ -3,10 +3,15 @@ package edu.quinnipiac.ser210.thirstmate;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +28,9 @@ public class ShoppingListFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    String[] data = {"Vodka", "Ginger Beer", "Lime Juice", "Simple Syrup", "Ice"};
+    int counter = 0;
 
     public ShoppingListFragment() {
         // Required empty public constructor
@@ -53,12 +61,30 @@ public class ShoppingListFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_shopping_list, container, false);
+
+        List<String> ingredients = new LinkedList<>();
+        ingredients.add("Code It");
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycleViewShopping);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        ShoppingListAdapter adapter = new ShoppingListAdapter(ingredients);
+        recyclerView.setAdapter(adapter);
+
+        view.findViewById(R.id.addIngredient).setOnClickListener(view1 -> {
+            ingredients.add(data[counter%5]);
+            counter++;
+            adapter.notifyItemInserted(ingredients.size()-1);
+        });
+
+        return view;
     }
 }
