349fefa5436c430f1b88987d394821b99dc55924
jjtheall <jtheall@quinnipiac.edu>
2022-04-19 21:53:42 -0400

    tab layout and viewPager built

--- app/src/main/java/edu/quinnipiac/ser210/thirstmate/HomepageFragment.java
+++ app/src/main/java/edu/quinnipiac/ser210/thirstmate/HomepageFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,6 +27,7 @@ public class HomepageFragment extends Fragment {
 
     public HomepageFragment() {
         // Required empty public constructor
+        Log.d("help","inside homepageFragment constructor");
     }
 
     /**
