990e652b75c95ec76091f67183fd9d6bdd497e06
jjtheall <jtheall@quinnipiac.edu>
2022-04-19 20:03:47 -0400

    refactored drinkView, info, settings, and splash frags to activities. Also refactored nav graph and changed manifest so that app boots to splashActivity

---
+++ app/src/main/java/edu/quinnipiac/ser210/thirstmate/DrinkViewActivity.java
@@ -0,0 +1,14 @@
+package edu.quinnipiac.ser210.thirstmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrinkViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_view);
+    }
+}
---
+++ app/src/main/java/edu/quinnipiac/ser210/thirstmate/SplashActivity.java
@@ -0,0 +1,14 @@
+package edu.quinnipiac.ser210.thirstmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
