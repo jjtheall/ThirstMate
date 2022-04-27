194454ffe91c250ea36afb34d3e8dd819fc40324
jjtheall <jtheall@quinnipiac.edu>
2022-04-27 00:51:39 -0400

    toolbar added to DrinkViewActivity and reformatted layout

--- app/src/main/java/edu/quinnipiac/ser210/thirstmate/DrinkViewActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/thirstmate/DrinkViewActivity.java
@@ -1,15 +1,22 @@
 package edu.quinnipiac.ser210.thirstmate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DrinkViewActivity extends AppCompatActivity {
+
+    private ShareActionProvider shareActionProvider;
 
     public static final String EXTRA_DRINKNAME = "drinkID";
     public static final String EXTRA_IMGRESID = "imageResourceId";
@@ -32,11 +39,13 @@ public class DrinkViewActivity extends AppCompatActivity {
     private String ing14Name = "";
     private String ing15Name = "";
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_view);
+
+        androidx.appcompat.widget.Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Intent intent = getIntent();
         String drinkName = intent.getStringExtra(EXTRA_DRINKNAME);
@@ -247,6 +256,44 @@ public class DrinkViewActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        // Inflate the menu; this adds items to the app bar
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent("ingredients list");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //change this method to accept list of ingredients
+    public void setShareActionIntent(String text){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,text);
+        shareActionProvider.setShareIntent(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.action_info:
+                Intent infoIntent = new Intent(this,InfoActivity.class);
+                startActivity(infoIntent);
+                return true;
+            case R.id.action_home:
+                Intent homeIntent = new Intent(this,MainActivity.class);
+                startActivity(homeIntent);
+                return true;
+            case R.id.action_settings:
+                Intent settingsIntent = new Intent(this,SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     public void onSubtractDrinkQuantity(View view){
         TextView drinkQuantity = (TextView) findViewById(R.id.drink_quantity);
         int curNum = Integer.parseInt(String.valueOf(drinkQuantity.getText()));
@@ -259,5 +306,9 @@ public class DrinkViewActivity extends AppCompatActivity {
         TextView drinkQuantity = (TextView) findViewById(R.id.drink_quantity);
         int curNum = Integer.parseInt(String.valueOf(drinkQuantity.getText()));
         drinkQuantity.setText("" + (curNum+1));
+    }
+
+    public void onAddIngredients(View view){
+
     }
 }
