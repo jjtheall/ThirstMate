b692a2c5e01bc65e36bc42e2f863202539c6e09a
jjtheall <jtheall@quinnipiac.edu>
2022-05-03 23:39:21 -0400

    layout cleanup & error handling

--- app/src/main/java/edu/quinnipiac/ser210/thirstmate/DrinkViewActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/thirstmate/DrinkViewActivity.java
@@ -191,91 +191,121 @@ public class DrinkViewActivity extends AppCompatActivity {
             ing1TextView.setText(ing1Name);
             ing1AmtTextView.setText(""+ing1Amt);
             ing1TextView.setVisibility(View.VISIBLE);
-            ing1AmtTextView.setVisibility(View.VISIBLE);
+            if(ing1Amt != 0){
+                ing1AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing2Name.equals("")){
             ing2TextView.setText(ing2Name);
             ing2AmtTextView.setText(""+ing2Amt);
             ing2TextView.setVisibility(View.VISIBLE);
-            ing2AmtTextView.setVisibility(View.VISIBLE);
+            if(ing2Amt != 0){
+                ing2AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing3Name.equals("")){
             ing3TextView.setText(ing3Name);
             ing3AmtTextView.setText(""+ing3Amt);
             ing3TextView.setVisibility(View.VISIBLE);
-            ing3AmtTextView.setVisibility(View.VISIBLE);
+            if(ing3Amt != 0){
+                ing3AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing4Name.equals("")){
             ing4TextView.setText(ing4Name);
             ing4AmtTextView.setText(""+ing4Amt);
             ing4TextView.setVisibility(View.VISIBLE);
-            ing4AmtTextView.setVisibility(View.VISIBLE);
+            if(ing4Amt != 0){
+                ing4AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing5Name.equals("")){
             ing5TextView.setText(ing5Name);
             ing5AmtTextView.setText(""+ing5Amt);
             ing5TextView.setVisibility(View.VISIBLE);
-            ing5AmtTextView.setVisibility(View.VISIBLE);
+            if(ing5Amt != 0){
+                ing5AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing6Name.equals("")){
             ing6TextView.setText(ing6Name);
             ing6AmtTextView.setText(""+ing6Amt);
             ing6TextView.setVisibility(View.VISIBLE);
-            ing6AmtTextView.setVisibility(View.VISIBLE);
+            if(ing6Amt != 0){
+                ing6AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing7Name.equals("")){
             ing7TextView.setText(ing7Name);
             ing7AmtTextView.setText(""+ing7Amt);
             ing7TextView.setVisibility(View.VISIBLE);
-            ing7AmtTextView.setVisibility(View.VISIBLE);
+            if(ing7Amt != 0){
+                ing7AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing8Name.equals("")){
             ing8TextView.setText(ing8Name);
             ing8AmtTextView.setText(""+ing8Amt);
             ing8TextView.setVisibility(View.VISIBLE);
-            ing8AmtTextView.setVisibility(View.VISIBLE);
+            if(ing8Amt != 0){
+                ing8AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing9Name.equals("")){
             ing9TextView.setText(ing9Name);
             ing9AmtTextView.setText(""+ing9Amt);
             ing9TextView.setVisibility(View.VISIBLE);
-            ing9AmtTextView.setVisibility(View.VISIBLE);
+            if(ing9Amt != 0){
+                ing9AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing10Name.equals("")){
             ing10TextView.setText(ing10Name);
             ing10AmtTextView.setText(""+ing10Amt);
             ing10TextView.setVisibility(View.VISIBLE);
-            ing10AmtTextView.setVisibility(View.VISIBLE);
+            if(ing10Amt != 0){
+                ing10AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing11Name.equals("")){
             ing11TextView.setText(ing11Name);
             ing11AmtTextView.setText(""+ing11Amt);
             ing11TextView.setVisibility(View.VISIBLE);
-            ing11AmtTextView.setVisibility(View.VISIBLE);
+            if(ing11Amt != 0){
+                ing11AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing12Name.equals("")){
             ing12TextView.setText(ing12Name);
             ing12AmtTextView.setText(""+ing12Amt);
             ing12TextView.setVisibility(View.VISIBLE);
-            ing12AmtTextView.setVisibility(View.VISIBLE);
+            if(ing12Amt != 0){
+                ing12AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing13Name.equals("")){
             ing13TextView.setText(ing13Name);
             ing13AmtTextView.setText(""+ing13Amt);
             ing13TextView.setVisibility(View.VISIBLE);
-            ing13AmtTextView.setVisibility(View.VISIBLE);
+            if(ing13Amt != 0){
+                ing13AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing14Name.equals("")){
             ing14TextView.setText(ing14Name);
             ing14AmtTextView.setText(""+ing14Amt);
             ing14TextView.setVisibility(View.VISIBLE);
-            ing14AmtTextView.setVisibility(View.VISIBLE);
+            if(ing14Amt != 0){
+                ing14AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
         if(!ing15Name.equals("")){
             ing15TextView.setText(ing15Name);
             ing15AmtTextView.setText(""+ing15Amt);
             ing15TextView.setVisibility(View.VISIBLE);
-            ing15AmtTextView.setVisibility(View.VISIBLE);
+            if(ing15Amt != 0){
+                ing15AmtTextView.setVisibility(View.VISIBLE);
+            }
         }
     }
 
